9e94ed442d7a021cfb640f9e10e918fd376518d9
Rob McDonnell <rob.f.mcdonnell@outlook.com>
2017-09-27 16:50:12 -0400

    Trying this git thing

--- Swift Fun/ViewController.swift
+++ Swift Fun/ViewController.swift
@@ -4,17 +4,66 @@
 //
 //  Created by Rob McDonnell on 9/27/17.
 //  Copyright © 2017 Rob McDonnell. All rights reserved.
-//
+//  Gits are neat, I guess
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var buttonCount = 0
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+        buttonCount = buttonCount + 1
+        
+        if buttonCount >= 10 {
+            
+            
+            view.backgroundColor = UIColor.red
+            
+            myLabel.text = "You hit the button at least ten times!"
+        
+        }
+        
+        if buttonCount >= 15 {
+            
+            
+            view.backgroundColor = UIColor.green
+            
+            myLabel.text = "You hit the button at least fifteen times!"
+            
+        }
+        
+        if buttonCount >= 20 {
+            
+            
+            view.backgroundColor = UIColor.yellow
+            
+            myLabel.text = "You smashed that button at least twenty times, dawg!"
+            
+        }
+        
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        
     }
 
+   
+    
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
