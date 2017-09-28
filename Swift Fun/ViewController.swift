61797c12afa2e6dd6eeb6cb0604d2283bbbeb414
Rob McDonnell <rob.f.mcdonnell@outlook.com>
2017-09-28 15:56:39 -0400

    Janky Calculator. It can add or subtract two doubles.

--- Swift Fun/ViewController.swift
+++ Swift Fun/ViewController.swift
@@ -18,10 +18,35 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var bottomTextField: UITextField!
     
+    @IBOutlet weak var additionSwitch: UISwitch!
+    
+    
+    
+    
+    
     @IBAction func buttonTapped(_ sender: Any) {
         
-        print(topTextField.text!)
-        print(bottomTextField.text!)
+      //  print(topTextField.text!)
+      //  print(bottomTextField.text!)
+        
+        let addition = additionSwitch.isOn
+        
+        if addition {
+            let sum = Double(topTextField.text!)! + Double(bottomTextField.text!)!
+            myLabel.text = "\(topTextField.text!) + \(bottomTextField.text!) = \(sum)"
+        } else {
+            let sum = Double(topTextField.text!)! - Double(bottomTextField.text!)!
+            myLabel.text = "\(topTextField.text!) - \(bottomTextField.text!) = \(sum)"
+        }
+        
+        
+        
+       // myLabel.text = "The sum is... " + String(sum)
+        
+        
+        
+        
+        
         
     
         
